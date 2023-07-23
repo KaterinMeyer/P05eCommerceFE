724ac2b4a4c975139df31b0a2aeba516ccedd2c3
KaterinMeyer <katerinmeyer@gmail.com>
2023-07-23 13:31:01 -0400

    Se prepara front end para conectar con Backend

--- src/contexts/users/UserState.jsx
+++ src/contexts/users/UserState.jsx
@@ -1,7 +1,9 @@
+import { useReducer } from "react"
 import UserContext from "./UserContext"
+import userReducers from "./UserReduce"
 
 const UserState = (props) => {
-    const intialState ={
+    const intialState = {
         user: {
             id: null,
             fullName: null,
@@ -10,11 +12,45 @@ const UserState = (props) => {
         authStatus: false,
     }
 
-        return (
-            <UserContext.Provider>
-                {props.children}
-            </UserContext.Provider>
-        )
+    const [globalState, dispatch] = useReducer(userReducers, intialState)
+
+    const registerUser = async (dataForm) => {
+        try {
+            const res = await fetch("http://localhost:4500/auth/signup", { method: 'POST', headers: { "Content-type": 'application/json', "Accept": 'application/json' }, body: JSON.stringify(dataForm) })
+            const payload = await res.json()
+            dispatch({
+                type: "REGISTRO_EXITOSO",
+                payload: payload
+            })
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    const loginUser = async (dataForm) => {
+        try {
+            const res = await fetch("http://localhost:4500/auth/login", { method: 'POST', headers: { "Content-type": 'application/json', "Accept": 'application/json' }, body: JSON.stringify(dataForm) })
+            const payload = await res.json()
+            dispatch({
+                type: "LOGIN_EXITOSO",
+                payload: payload
+            })
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+
+    return (
+        <UserContext.Provider value={{
+            ...globalState,
+            registerUser,
+            loginUser
+        }}>
+            {props.children}
+        </UserContext.Provider>
+    )
 }
 
 export default UserState
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,35 +1,43 @@
+import { useContext } from 'react';
 import { Container } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
+import UserContext from '../../contexts/users/UserContext';
 
-export function Home () {
-    return(
-<Container>
-    <Carousel fade>
-        <Carousel.Item>
-            <img 
-            className = "d-block w-90"
-            src='../src/assets/typischedeutschegerichte.jpg' />
-            <Carousel.Caption>
-                <h3>Comida tradicional alemana</h3>
-                <p>Mucha variedad</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-        <img src='../src/assets/Rezept_Linsen&Spätzle.jpg' />
-            <Carousel.Caption>
-                <h3>Platos tradicionales</h3>
-                <p>Para todos los gustos</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-        <img src='../src/assets/Goulasch.jpg' />
-            <Carousel.Caption>
-                <h3>Lo mejor de la región</h3>
-                <p>Deléitese con la comida alemana de verdad</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-    </Carousel>
+export function Home() {
+    const userCtx = useContext(UserContext)
+    const { user } = userCtx
+    const { fullName } = user
+    return (
 
-</Container>
-)}
+        <Container>
+            <h1>{`Bienvenido ${fullName}`}</h1>
+            <Carousel fade>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-90"
+                        src='../src/assets/typischedeutschegerichte.jpg' />
+                    <Carousel.Caption>
+                        <h3>Comida tradicional alemana</h3>
+                        <p>Mucha variedad</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src='../src/assets/Rezept_Linsen&Spätzle.jpg' />
+                    <Carousel.Caption>
+                        <h3>Platos tradicionales</h3>
+                        <p>Para todos los gustos</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src='../src/assets/Goulasch.jpg' />
+                    <Carousel.Caption>
+                        <h3>Lo mejor de la región</h3>
+                        <p>Deléitese con la comida alemana de verdad</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+
+        </Container>
+    )
+}
 export default Home
