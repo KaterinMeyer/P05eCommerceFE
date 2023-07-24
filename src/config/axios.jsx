a0f0c94cbc1f8cb403f1c0d66c88ab5fff85bb95
KaterinMeyer <katerinmeyer@gmail.com>
2023-07-23 21:06:05 -0400

    correcciones para que funcione el deploy

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const backendUrlLocal = "http://localhost:4500"
-const backendUrlProd = "https://proyecto05ecommerce2023.onrender.com"
+const backendUrlProd = "https://proyecto05ecommerce2023.onrender.com/"
 const axiosClient = axios.create( {
-    baseURL: backendUrlLocal
+    baseURL: backendUrlProd
   } )
   
   export default axiosClient
